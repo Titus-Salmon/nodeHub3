4ce1b8318b9cccd5eb29c1ef1a1713d34ed9fe34
tsalm0n <tsalm0n@twc.com>
2020-09-13 19:52:16 -0400

    1st commit from cloned home repo

--- sqlArch/venProf.js
+++ sqlArch/venProf.js
@@ -24,52 +24,52 @@ var htmlStub = `<div id="dataviz-container"></div>` // html file skull with a co
 module.exports = {
   venProf: router.post('/venProf', (req, res, next) => {
 
-    // jsdom.env({
-    //   features: {
-    //     QuerySelector: true
-    //   },
-    //   html: htmlStub,
-    //   done: function (errors, window) {
-    //     // this callback function pre-renders the dataviz inside the html document, then export result into a static file
+    jsdom.env({
+      features: {
+        QuerySelector: true
+      },
+      html: htmlStub,
+      done: function (errors, window) {
+        // this callback function pre-renders the dataviz inside the html document, then export result into a static file
 
-    //     var el = window.document.querySelector('#dataviz-container'),
-    //       body = window.document.querySelector('body'),
-    //       circleId = 'a2324' // say, this value was dynamically retrieved from some database
+        var el = window.document.querySelector('#dataviz-container'),
+          body = window.document.querySelector('body'),
+          circleId = 'a2324' // say, this value was dynamically retrieved from some database
 
-    //     // generate the dataviz
-    //     d3.select(el)
-    //       .append('svg:svg')
-    //       .attr('width', 600)
-    //       .attr('height', 300)
-    //       .append('circle')
-    //       .attr('cx', 300)
-    //       .attr('cy', 150)
-    //       .attr('r', 30)
-    //       .attr('fill', '#26963c')
-    //       .attr('id', circleId) // say, this value was dynamically retrieved from some database
+        // generate the dataviz
+        d3.select(el)
+          .append('svg:svg')
+          .attr('width', 600)
+          .attr('height', 300)
+          .append('circle')
+          .attr('cx', 300)
+          .attr('cy', 150)
+          .attr('r', 30)
+          .attr('fill', '#26963c')
+          .attr('id', circleId) // say, this value was dynamically retrieved from some database
 
-    //     // make the client-side script manipulate the circle at client side)
-    //     var clientScript = "d3.select('#" + circleId + "').transition().delay(1000).attr('fill', '#f9af26')"
+        // make the client-side script manipulate the circle at client side)
+        var clientScript = "d3.select('#" + circleId + "').transition().delay(1000).attr('fill', '#f9af26')"
 
-    //     d3.select(body)
-    //       .append('script')
-    //       .html(clientScript)
+        d3.select(body)
+          .append('script')
+          .html(clientScript)
 
-    //     // save result in an html file, we could also keep it in memory, or export the interesting fragment into a database for later use
-    //     var svgsrc = window.document.innerHTML
-    //     fs.writeFile(process.cwd() + '/views/inserts/venProfResults.html', svgsrc, function (err) {
-    //       if (err) {
-    //         console.log('error saving document', err)
-    //       } else {
-    //         console.log('The file was saved!')
-    //         res.render('vw-venProf', {
-    //           title: `Monthly profits by vendor: ${vendorName}`,
-    //           venProfArr: venProfArr,
-    //         })
-    //       }
-    //     })
-    //   } // end jsDom done callback
-    // })
+        // save result in an html file, we could also keep it in memory, or export the interesting fragment into a database for later use
+        var svgsrc = window.document.innerHTML
+        fs.writeFile(process.cwd() + '/views/inserts/venProfResults.html', svgsrc, function (err) {
+          if (err) {
+            console.log('error saving document', err)
+          } else {
+            console.log('The file was saved!')
+            res.render('vw-venProf', {
+              title: `Monthly profits by vendor: ${vendorName}`,
+              venProfArr: venProfArr,
+            })
+          }
+        })
+      } // end jsDom done callback
+    })
 
     let vendorName = req.body['vendorNamePost']
     console.log(`vendorName==> ${vendorName}`)
