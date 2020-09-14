2524d18493bf8d82ab0cd8b2f5ff06342f845ade
tsalm0n <tsalm0n@twc.com>
2020-09-13 22:22:51 -0400

    data viz stuff via d3 & jsdom

---
+++ jsDomScriptsT0d/jsdomScript1.js
@@ -0,0 +1,37 @@
+var el = window.document.querySelector('#dataviz-container'),
+  body = window.document.querySelector('body'),
+  circleId = 'a2324' // say, this value was dynamically retrieved from some database
+
+// generate the dataviz
+d3.select(el)
+  .append('svg:svg')
+  .attr('width', 600)
+  .attr('height', 300)
+  .append('circle')
+  .attr('cx', 300)
+  .attr('cy', 150)
+  .attr('r', 30)
+  .attr('fill', '#26963c')
+  .attr('id', circleId) // say, this value was dynamically retrieved from some database
+
+// make the client-side script manipulate the circle at client side)
+var clientScript = "d3.select('#" + circleId + "').transition().delay(1000).attr('fill', '#f9af26')"
+
+d3.select(body)
+  .append('script')
+  .html(clientScript)
+
+// save result in an html file, we could also keep it in memory, or export the interesting fragment into a database for later use
+var svgsrc = window.document.innerHTML
+console.log(`process.cwd()==> ${process.cwd()}`)
+fs.writeFile(`${process.cwd()}../views/inserts/venProfResults.html`, svgsrc, function (err) {
+  if (err) {
+    console.log('error saving document', err)
+  } else {
+    console.log('The file was saved!')
+    res.render('vw-venProf', {
+      title: `Monthly profits by vendor: ${vendorName}`,
+      venProfArrDisplay: venProfArr,
+    })
+  }
+})
--- sqlArch/venProf.js
+++ sqlArch/venProf.js
@@ -110,54 +110,65 @@ module.exports = {
       // })
     })
 
-    //v////d3/jsdom stuff////////////////////////////////////////////////////////
-    jsdom.env({
-      features: {
-        QuerySelector: true
-      },
-      html: htmlStub,
-      done: function (errors, window) {
-        // this callback function pre-renders the dataviz inside the html document, then export result into a static file
-
-        var el = window.document.querySelector('#dataviz-container'),
-          body = window.document.querySelector('body'),
-          circleId = 'a2324' // say, this value was dynamically retrieved from some database
-
-        // generate the dataviz
-        d3.select(el)
-          .append('svg:svg')
-          .attr('width', 600)
-          .attr('height', 300)
-          .append('circle')
-          .attr('cx', 300)
-          .attr('cy', 150)
-          .attr('r', 30)
-          .attr('fill', '#26963c')
-          .attr('id', circleId) // say, this value was dynamically retrieved from some database
-
-        // make the client-side script manipulate the circle at client side)
-        var clientScript = "d3.select('#" + circleId + "').transition().delay(1000).attr('fill', '#f9af26')"
-
-        d3.select(body)
-          .append('script')
-          .html(clientScript)
-
-        // save result in an html file, we could also keep it in memory, or export the interesting fragment into a database for later use
-        var svgsrc = window.document.innerHTML
-        fs.writeFile(process.cwd() + '/views/inserts/venProfResults.html', svgsrc, function (err) {
-          if (err) {
-            console.log('error saving document', err)
-          } else {
-            console.log('The file was saved!')
-            res.render('vw-venProf', {
-              title: `Monthly profits by vendor: ${vendorName}`,
-              venProfArrDisplay: venProfArr,
-            })
-          }
-        })
-      } // end jsDom done callback
+    const jsdomT0d = new JSDOM(``, {
+      runScripts: "dangerously"
     })
-    //^////d3/jsdom stuff////////////////////////////////////////////////////////
+    const myLibrary = fs.readFileSync(`${process.cwd()}../jsDomScriptsT0d/jsdomScript1.js`, {
+      encoding: "utf-8"
+    })
+    const jsdomScriptElement = jsdomT0d.document.createElement("script")
+    jsdomScriptElement.textContent = myLibrary
+    jsdomT0d.document.body.appendChild(jsdomScriptElement)
+
+    // //v////d3/jsdom stuff////////////////////////////////////////////////////////
+    // const jsdomT0d = new JSDOM(``, { runScripts: "dangerously" })
+    // jsdom.env({
+    //   features: {
+    //     QuerySelector: true
+    //   },
+    //   html: htmlStub,
+    //   done: function (errors, window) {
+    //     // this callback function pre-renders the dataviz inside the html document, then export result into a static file
+
+    //     var el = window.document.querySelector('#dataviz-container'),
+    //       body = window.document.querySelector('body'),
+    //       circleId = 'a2324' // say, this value was dynamically retrieved from some database
+
+    //     // generate the dataviz
+    //     d3.select(el)
+    //       .append('svg:svg')
+    //       .attr('width', 600)
+    //       .attr('height', 300)
+    //       .append('circle')
+    //       .attr('cx', 300)
+    //       .attr('cy', 150)
+    //       .attr('r', 30)
+    //       .attr('fill', '#26963c')
+    //       .attr('id', circleId) // say, this value was dynamically retrieved from some database
+
+    //     // make the client-side script manipulate the circle at client side)
+    //     var clientScript = "d3.select('#" + circleId + "').transition().delay(1000).attr('fill', '#f9af26')"
+
+    //     d3.select(body)
+    //       .append('script')
+    //       .html(clientScript)
+
+    //     // save result in an html file, we could also keep it in memory, or export the interesting fragment into a database for later use
+    //     var svgsrc = window.document.innerHTML
+    //     fs.writeFile(process.cwd() + '/views/inserts/venProfResults.html', svgsrc, function (err) {
+    //       if (err) {
+    //         console.log('error saving document', err)
+    //       } else {
+    //         console.log('The file was saved!')
+    //         res.render('vw-venProf', {
+    //           title: `Monthly profits by vendor: ${vendorName}`,
+    //           venProfArrDisplay: venProfArr,
+    //         })
+    //       }
+    //     })
+    //   } // end jsDom done callback
+    // })
+    // //^////d3/jsdom stuff////////////////////////////////////////////////////////
 
   })
 }
