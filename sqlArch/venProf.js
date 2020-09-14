854d5953c6c164419a215b4ce02f98ecc63f172d
tsalm0n <tsalm0n@twc.com>
2020-09-13 21:14:41 -0400

    data viz stuff via d3 & jsdom

--- sqlArch/venProf.js
+++ sqlArch/venProf.js
@@ -24,6 +24,93 @@ var htmlStub = `<div id="dataviz-container"></div>` // html file skull with a co
 module.exports = {
   venProf: router.post('/venProf', (req, res, next) => {
 
+    // jsdom.env({
+    //   features: {
+    //     QuerySelector: true
+    //   },
+    //   html: htmlStub,
+    //   done: function (errors, window) {
+    //     // this callback function pre-renders the dataviz inside the html document, then export result into a static file
+
+    //     var el = window.document.querySelector('#dataviz-container'),
+    //       body = window.document.querySelector('body'),
+    //       circleId = 'a2324' // say, this value was dynamically retrieved from some database
+
+    //     // generate the dataviz
+    //     d3.select(el)
+    //       .append('svg:svg')
+    //       .attr('width', 600)
+    //       .attr('height', 300)
+    //       .append('circle')
+    //       .attr('cx', 300)
+    //       .attr('cy', 150)
+    //       .attr('r', 30)
+    //       .attr('fill', '#26963c')
+    //       .attr('id', circleId) // say, this value was dynamically retrieved from some database
+
+    //     // make the client-side script manipulate the circle at client side)
+    //     var clientScript = "d3.select('#" + circleId + "').transition().delay(1000).attr('fill', '#f9af26')"
+
+    //     d3.select(body)
+    //       .append('script')
+    //       .html(clientScript)
+
+    //     // save result in an html file, we could also keep it in memory, or export the interesting fragment into a database for later use
+    //     var svgsrc = window.document.innerHTML
+    //     fs.writeFile(process.cwd() + '/views/inserts/venProfResults.html', svgsrc, function (err) {
+    //       if (err) {
+    //         console.log('error saving document', err)
+    //       } else {
+    //         console.log('The file was saved!')
+    //         res.render('vw-venProf', {
+    //           title: `Monthly profits by vendor: ${vendorName}`,
+    //           venProfArrDisplay: venProfArr,
+    //         })
+    //       }
+    //     })
+    //   } // end jsDom done callback
+    // })
+
+    let vendorName = req.body['vendorNamePost']
+    console.log(`vendorName==> ${vendorName}`)
+
+    let venProfArr = []
+
+    function displayvenProf(rows) {
+      for (let i = 0; i < rows.length; i++) {
+        let venProfObj = {}
+        venProfObj['ri_t0d'] = i + 1
+        venProfObj['date'] = rows[i]['date']
+        venProfObj['kehe'] = rows[i]['kehe']
+
+        venProfArr.push(venProfObj)
+      }
+      venProfArrCache.set('venProfArrCache_key', venProfArr)
+      console.log('rows.length~~~>', rows.length)
+      // console.log(`Object.keys(rows)==>${Object.keys(rows)}`)
+      console.log(`Object.keys(rows[0])==>${Object.keys(rows[0])}`)
+    }
+
+
+    connection.query(`
+    SELECT date, ${vendorName} FROM ois_venprof_mnth
+    `, function (err, rows, fields) {
+      if (err) throw err
+      console.log(`rows.length==>${rows.length}`)
+      console.log('rows[0]==>', rows[0])
+      // console.log(`rows[0]['invScanCode']==>${rows[0]['invScanCode']}`)
+      // console.log(`rows[0]['invName']==>${rows[0]['invName']}`)
+      // console.log('rows==>', rows)
+      // res.send(rows)
+      displayvenProf(rows)
+
+      // res.render('vw-venProf', {
+      //   title: `Monthly profits by vendor: ${vendorName}`,
+      //   venProfArr: venProfArr,
+      // })
+    })
+
+    //v////d3/jsdom stuff////////////////////////////////////////////////////////
     jsdom.env({
       features: {
         QuerySelector: true
@@ -70,45 +157,7 @@ module.exports = {
         })
       } // end jsDom done callback
     })
-
-    let vendorName = req.body['vendorNamePost']
-    console.log(`vendorName==> ${vendorName}`)
-
-    let venProfArr = []
-
-    function displayvenProf(rows) {
-      for (let i = 0; i < rows.length; i++) {
-        let venProfObj = {}
-        venProfObj['ri_t0d'] = i + 1
-        venProfObj['date'] = rows[i]['date']
-        venProfObj['kehe'] = rows[i]['kehe']
-
-        venProfArr.push(venProfObj)
-      }
-      venProfArrCache.set('venProfArrCache_key', venProfArr)
-      console.log('rows.length~~~>', rows.length)
-      // console.log(`Object.keys(rows)==>${Object.keys(rows)}`)
-      console.log(`Object.keys(rows[0])==>${Object.keys(rows[0])}`)
-    }
-
-
-    connection.query(`
-    SELECT date, ${vendorName} FROM ois_venprof_mnth
-    `, function (err, rows, fields) {
-      if (err) throw err
-      console.log(`rows.length==>${rows.length}`)
-      console.log('rows[0]==>', rows[0])
-      // console.log(`rows[0]['invScanCode']==>${rows[0]['invScanCode']}`)
-      // console.log(`rows[0]['invName']==>${rows[0]['invName']}`)
-      // console.log('rows==>', rows)
-      // res.send(rows)
-      displayvenProf(rows)
-
-      // res.render('vw-venProf', {
-      //   title: `Monthly profits by vendor: ${vendorName}`,
-      //   venProfArr: venProfArr,
-      // })
-    })
+    //^////d3/jsdom stuff////////////////////////////////////////////////////////
 
   })
 }
