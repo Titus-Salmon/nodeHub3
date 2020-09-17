2d985b5c9ba130ed5d705d5016c5bdaad61cd176
tsalm0n <tsalm0n@twc.com>
2020-09-17 15:42:40 -0400

    d3 line chart troubleshoot

--- sqlArch/venProf.js
+++ sqlArch/venProf.js
@@ -72,6 +72,8 @@ module.exports = {
 
     async function writeHTMLfileAndRenderPage() {
       var svgsrc = jsdomT0d.window.document.documentElement.innerHTML
+      console.log(`jsdomT0d1==> ${jsdomT0d}`)
+      console.log(`svgsrc==> ${svgsrc}`)
       fs.writeFile(`${process.cwd()}/views/includes/venProfResults.html`, svgsrc, function (err) {
         if (err) {
           console.log('error saving document', err)
