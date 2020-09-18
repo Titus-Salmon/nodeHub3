5634ea3c2d187e38ceb0fe0c6e82e5be1b6d6258
tsalm0n <tsalm0n@twc.com>
2020-09-17 22:31:35 -0400

    d3 line chart setup

--- sqlArch/venProf.js
+++ sqlArch/venProf.js
@@ -146,42 +146,21 @@ module.exports = {
           .text(data.y))
       console.log(`yAxis==> ${yAxis}`)
 
-      const svg = d3.create("svg")
-        .attr("viewBox", [0, 0, width, height]);
-
-      svg.append("g")
-        .call(xAxis);
-
-      svg.append("g")
-        .call(yAxis);
-
-      svg.append("path")
+      d3.select(el)
+        .append('svg')
+        .attr("viewBox", [0, 100, width, height])
+        .append("g")
+        .call(xAxis)
+        .append("g")
+        .call(yAxis)
+        .append("path")
         .datum(data)
         .attr("fill", "none")
         .attr("stroke", "steelblue")
         .attr("stroke-width", 1.5)
         .attr("stroke-linejoin", "round")
         .attr("stroke-linecap", "round")
-        .attr("d", line);
-
-      d3.select(el)
-        .append('svg')
-
-      // d3.select(el)
-      //   .append('svg')
-      //   .attr("viewBox", [0, 0, width, height])
-      //   .append("g")
-      //   .call(xAxis)
-      //   .append("g")
-      //   .call(yAxis)
-      //   .append("path")
-      //   .datum(data)
-      //   .attr("fill", "none")
-      //   .attr("stroke", "steelblue")
-      //   .attr("stroke-width", 1.5)
-      //   .attr("stroke-linejoin", "round")
-      //   .attr("stroke-linecap", "round")
-      //   .attr("d", line)
+        .attr("d", line)
 
       console.log(`JSON.stringify(venProfArr) from createLineChartT0d()==> ${JSON.stringify(venProfArr)}`)
     }
