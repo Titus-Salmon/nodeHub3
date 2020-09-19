5dacc11ea7b2b5f4c19f205e22d3d91e74d59016
tsalm0n <tsalm0n@twc.com>
2020-09-18 20:10:24 -0400

    d3 line chart setup

--- sqlArch/venProf.js
+++ sqlArch/venProf.js
@@ -60,7 +60,7 @@ module.exports = {
 
     console.log(`JSON.stringify(venProfArr)==> ${JSON.stringify(venProfArr)}`)
 
-    const jsdomT0d = new JSDOM(`<!DOCTYPE html><body><div id="dataviz-container" style="margin: 20px 20px 350px 20px"></div></body>`)
+    const jsdomT0d = new JSDOM(`<!DOCTYPE html><body><div id="dataviz-container" style="margin: 20px 20px 600px 20px"></div></body>`)
 
     var el = jsdomT0d.window.document.querySelector('#dataviz-container')
 
