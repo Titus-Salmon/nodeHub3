d6fad5c3f4f57443b44205c5828f5ea31f5f7849
tsalm0n <tsalm0n@twc.com>
2020-09-20 16:34:12 -0400

    add tracking for WS & Rtl updates to venProf graphs

--- sqlArch/venProf.js
+++ sqlArch/venProf.js
@@ -247,7 +247,7 @@ module.exports = {
 
       var yAxisUpdateDemarcator = g => g
         .attr("transform", `translate(${timeScaleUpdateDemarcator(minWSdate)-10},0)`)
-        .call(d3.axisRight(yWS))
+        .call(d3.axisRight(y))
         .call(g => g.select(".domain").remove())
         .call(g => g.select(".tick:last-of-type text").clone()
           .attr("x", 3)
