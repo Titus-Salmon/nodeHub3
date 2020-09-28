359647cd2167fa6022c4cf7710bf312c6e13bc39
tsalm0n <tsalm0n@twc.com>
2020-09-28 16:53:01 -0400

    start forward geocoding module

--- sqlArch/custPlusAddr/custPlusAddr.js
+++ sqlArch/custPlusAddr/custPlusAddr.js
@@ -64,11 +64,11 @@ module.exports = {
             console.log(`JSON.stringify(gcdrResults)==> ${JSON.stringify(gcdrResults)}`)
             for (let i = 0; i < gcdrResults.length; i++) {
                 var latLongArr = gcdrResults.map(function (thingy_t0d) {
-                    // return [thingy_t0d['value'][0]['latitude'], thingy_t0d['value'][0]['longitude']]
-                    return [{
-                        'lat_t0d': thingy_t0d['value']['latitude'],
-                        'long_t0d': thingy_t0d['value']['longitude']
-                    }]
+                    return [thingy_t0d['value'][0]['latitude'], thingy_t0d['value'][0]['longitude']]
+                    // return [{
+                    //     'lat_t0d': thingy_t0d['value']['latitude'],
+                    //     'long_t0d': thingy_t0d['value']['longitude']
+                    // }]
                 })
                 console.log(`latLongArr==> ${latLongArr}`)
                 console.log(`JSON.stringify(latLongArr)==> ${JSON.stringify(latLongArr)}`)
