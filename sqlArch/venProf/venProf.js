0d0d47ecdef93a54642f65e2a9af24772127f1c9
tsalm0n <tsalm0n@twc.com>
2020-10-05 22:43:11 -0400

    add covid demarcator to venProf module

--- sqlArch/venProf/venProf.js
+++ sqlArch/venProf/venProf.js
@@ -291,9 +291,11 @@ module.exports = {
 
       svg.append("text")
         .text(`${covidData[0].notes}`)
-        .attr("transform", "rotate(-15)")
+        .attr("text-anchor", "start")
         .attr("x", covidData[0].date)
         .attr("y", covidData[0].yValue)
+        .attr("transform", "rotate(-15)")
+
         // .attr("transform", "rotate(-5)")
         // .attr("fill", "red")
         .style('font-size', '8px')
