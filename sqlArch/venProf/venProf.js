acd6a48f6d458b45aefb532482cde61c200c4788
tsalm0n <tsalm0n@twc.com>
2020-10-08 01:42:56 -0400

    add handling for sales data to venProf module

--- sqlArch/venProf/venProf.js
+++ sqlArch/venProf/venProf.js
@@ -224,7 +224,7 @@ module.exports = {
 
       svg.append("rect")
         .attr("x", -25)
-        .attr("y", 0)
+        .attr("y", xAxis_yValue)
         .attr("width", 50)
         .attr("height", height)
         .attr("fill", "orange")
