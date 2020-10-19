4d233c17b96da3714b706654ebd5aa0cd27dbeb2
tsalm0n <tsalm0n@twc.com>
2020-10-19 17:05:51 -0400

    cleanup/refactoring

--- funcLibT0d/calcResFormInputs.js
+++ funcLibT0d/calcResFormInputs.js
@@ -261,10 +261,9 @@ module.exports = {
     console.log(`formInputsObj.deptFilterToApply==> ${formInputsObj.deptFilterToApply}`)
     console.log(`calcResFormInputs says: JSON.stringify(formInputsObj)==> ${JSON.stringify(formInputsObj)}`)
 
-    console.log(`postBodyData.length==> ${postBodyData.length}`)
     console.log(`Object.keys(postBodyData).length==> ${Object.keys(postBodyData).length}`)
 
-    for (let i = 0; i < postBodyData.length; i++) {
+    for (let i = 0; i < Object.keys(postBodyData).length; i++) {
       console.log(`Object.keys(postBodyData)[${i}] **>> Object.values(postBodyData)[${i}]==> ${Object.keys(postBodyData)[i]} **>> ${Object.values(postBodyData)[i]}`)
     }
 
